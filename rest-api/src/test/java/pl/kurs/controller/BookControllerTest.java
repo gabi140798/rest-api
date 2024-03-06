e8a187da4c7ac4016ff4a5de904afe92d2c9ede3
michal <ttt.ttt2347@gmail.com>
2024-03-06 18:22:40 +0100

    Zaj 3 pd testy

--- rest-api/src/test/java/pl/kurs/controller/BookControllerTest.java
+++ rest-api/src/test/java/pl/kurs/controller/BookControllerTest.java
@@ -13,12 +13,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import pl.kurs.Main;
 import pl.kurs.model.Book;
 import pl.kurs.model.command.CreateBookCommand;
+import pl.kurs.model.command.EditBookCommand;
 import pl.kurs.service.BookIdGenerator;
 
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -56,8 +57,8 @@ class BookControllerTest {
         String json = objectMapper.writeValueAsString(command);
 
         postman.perform(post("/api/v1/books")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.title").value("testowa"))
@@ -68,11 +69,81 @@ class BookControllerTest {
 
         Assertions.assertEquals("testowa", recentlyAdded.getTitle());
         Assertions.assertEquals("TESTOWA", recentlyAdded.getCategory());
-        Assertions.assertTrue( recentlyAdded.isAvailable());
+        Assertions.assertTrue(recentlyAdded.isAvailable());
 
         Mockito.verify(bookIdGenerator, Mockito.times(1)).getId();
     }
 
     // napisac testy do pozostałych metod
-  
+
+    @Test
+    public void shoudFindBook() throws Exception {
+        int bookId = 1;
+
+        postman.perform(get("/api/v1/books/{id}", bookId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(bookId))
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.category").exists())
+                .andExpect(jsonPath("$.available").exists());
+    }
+
+    @Test
+    public void shouldDeleteBook() throws Exception {
+        int bookId = 1;
+
+        postman.perform(delete("/api/v1/books/{id}", bookId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+        assertFalse(books.stream().anyMatch(b -> b.getId() == bookId));
+
+    }
+
+    @Test
+    public void shouldEditBook() throws Exception {
+        int bookId = 1;
+        EditBookCommand command = new EditBookCommand("new", "NEW", false);
+        String json = objectMapper.writeValueAsString(command);
+
+        postman.perform(put("/api/v1/books/{id}", bookId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(bookId))
+                .andExpect(jsonPath("$.title").value("new"))
+                .andExpect(jsonPath("$.category").value("NEW"))
+                .andExpect(jsonPath("$.available").value(false));
+
+        Book newBook = books.stream()
+                .filter(book -> book.getId() == bookId)
+                .findFirst()
+                .orElse(null);
+        assertNotNull(newBook);
+        assertEquals("new", newBook.getTitle());
+        assertEquals("NEW", newBook.getCategory());
+        assertFalse(newBook.isAvailable());
+    }
+
+    @Test
+    public void shouldEditBookPartially() throws Exception {
+        int bookId = 1;
+        EditBookCommand command = new EditBookCommand(null, "NEW", null);
+        String json = objectMapper.writeValueAsString(command);
+
+        postman.perform(patch("/api/v1/books/{id}", bookId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(bookId))
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.category").value("NEW"))
+                .andExpect(jsonPath("$.available").exists());
+
+        Book newBook = books.stream()
+                .filter(book -> book.getId() == bookId).findFirst().orElse(null);
+        assertNotNull(newBook);
+        assertEquals("NEW", newBook.getCategory());
+
+    }
 }
